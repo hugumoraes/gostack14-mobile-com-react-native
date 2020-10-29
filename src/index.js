060ef0935e8399cdadbd6180ef0dfe1767351be8
Hugo <hugomoraes_@hotmail.com>
2020-10-29 19:29:21 -0300

    Módulo finalizado

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+
+import api from './services/api';
+
+export default function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('/projects').then(r => {
+            setProjects(r.data);
+        }).catch(err => console.log(err));
+    }, []);
+
+    async function handleAddProject() {
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: 'Hugo Moraes Bonatto'
+        });
+
+        setProjects([...projects, response.data]);
+    }
+
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+            <SafeAreaView style={styles.container}>
+                <FlatList 
+                    data={projects}
+                    keyExtractor={project => project.id}
+                    renderItem={({ item: project }) => (
+                        <Text style={styles.project}>{project.title}</Text>
+                    )}
+                />
+
+                <TouchableOpacity 
+                    activeopacity={0.6} 
+                    style={styles.button} 
+                    onPress={handleAddProject}
+                >
+                    <Text style={styles.buttonText}>Adicionar projeto</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
+            
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+    },
+    project: {
+        color: '#fff',
+        fontSize: 20,
+    },
+    button: {
+        backgroundColor: '#fff',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
+});
